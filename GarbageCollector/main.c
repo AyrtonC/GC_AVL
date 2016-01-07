50b3ab454631d3a99ae61727338e1a5b798f56c8
Ayrton Cavalieri de Almeida <ayrtoncavalieri@gmail.com>
2016-01-06 22:31:22 -0200

    Initial Commit

---
+++ GarbageCollector/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GarbageCollector
+//
+//  Created by Ayrton Cavalieri on 6/01/2016.
+//  Copyright © 2016 AyrtonC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
